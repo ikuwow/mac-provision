bb82c321321dac90ec4366c28e4c0c0a4c7210cb
Ikuo Degawa <degawa@ikuwow.com>
2016-01-08 08:40:38 +0900

    Add script to create symlink of Google Japanese Input

---
+++ sync-gji.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+GSI_PATH='~/Library/Application\ Support/Google/JapaneseInput'
+
+rm -r ~/Library/Application\ Support/Google/JapaneseInput
+ln -ins ~/iCloud\ Drive/CloudDocs/GoogleJapaneseInput ~/Library/Application\ Support/Google/JapaneseInput
