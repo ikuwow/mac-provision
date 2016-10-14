55b92b5489328a707afc570ad2d97511f081c9a1
Ikuo Degawa <degawa@ikuwow.com>
2016-10-14 22:26:05 +0900

    sync ssh_config on the icloud

--- symlinks.sh
+++ symlinks.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
+# Sync Google Japanese Input dictionary
 GSI_PATH='~/Library/Application\ Support/Google/JapaneseInput'
-
 rm -r ~/Library/Application\ Support/Google/JapaneseInput
 ln -ins ~/iCloud\ Drive/CloudDocs/GoogleJapaneseInput ~/Library/Application\ Support/Google/JapaneseInput
+
+# Sync .ssh/config on iCloud Drive
+SSHCONF=~/iCloud\ Drive/CloudDocs/ssh/config
+if ( test -e "$SSHCONF" ) && ( test ! -L ~/.ssh/config ); then
+    ln -ins "$SSHCONF" ~/.ssh/config
+fi
+
